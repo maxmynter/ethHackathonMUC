025523a00f81bd51cff403648dae766a0505c315
owen <owen@owen>
2023-08-12 17:30:48 +0200

    basic layout

--- pages/linkmatch/index.tsx
+++ pages/linkmatch/index.tsx
@@ -3,7 +3,13 @@ const LinkMatchView = () => {
   return (
     <div>
       <Header />
-      <div>Link Match View</div>
+
+      <div className=" bg-slate-100">
+        <div className="content grid gap-4 grid-cols-2 grid-rows-1">
+          <div className="bg-white flex-1 w-full m-3">Job here</div>
+          <div className="bg-white flex-1 w-full m-3">Candidate here</div>
+        </div>
+      </div>
     </div>
   );
 };
