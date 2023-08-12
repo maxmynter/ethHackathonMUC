421868e843ccf7bfb001ff784ffc259f99c9cbd4
owen <owen@owen>
2023-08-12 18:01:51 +0200

    layout changes

--- components/header.tsx
+++ components/header.tsx
@@ -31,8 +31,8 @@ const Header = ({
   size?: number;
 }) => {
   return (
-    <div className="container mx-auto">
-      <div className="flex flex-row justify-center mt-10  w-full h-12">
+    <div className="container mx-auto h-20 flex flex-col justify-center">
+      <div className="flex flex-row justify-center w-full h-12">
         <div>
           <Image src="./linkedup.svg" width={150} height={80} alt="LinkedUp" />
         </div>
--- pages/linkmatch/index.tsx
+++ pages/linkmatch/index.tsx
@@ -1,15 +1,17 @@
 import Header from "../../components/header";
 const LinkMatchView = () => {
   return (
-    <div className="flex flex-col  h-full">
+    <div className="flex flex-col h-full">
       <div>
         <Header />
       </div>
 
-      <div className="mt-3 bg-slate-100 flex-1">
+      <div className=" bg-slate-100 flex-1 flex flex-row">
         <div className="container mx-auto grid gap-4 grid-cols-2 grid-rows-1">
-          <div className="bg-white flex-1 w-full m-3 p-3">Job here</div>
-          <div className="bg-white flex-1 w-full m-3 p-3">Candidate here</div>
+          <div className="bg-white flex-1 w-full h-full m-3 p-3">Job here</div>
+          <div className="bg-white flex-1 w-full h-full m-3 p-3">
+            Candidate here
+          </div>
         </div>
       </div>
 
