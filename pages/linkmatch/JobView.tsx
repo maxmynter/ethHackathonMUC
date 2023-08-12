8218dce019b6c76140d566d9f1b68b4e299ab3a8
owen <owen@owen>
2023-08-12 22:03:10 +0200

    matching stuff

--- pages/linkmatch/JobView.tsx
+++ pages/linkmatch/JobView.tsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
+import { ExpandedJobPosting } from "../../types/global";
 import mockData from "../../utils/mockData";
 
-const JobPost = ({ job }) => {
+const JobPost = ({
+  job,
+  selectedJob,
+}: {
+  job: ExpandedJobPosting;
+  selectedJob: ExpandedJobPosting;
+}) => {
   return (
     <div>
-      <div className="flex flex-col">
-        <h3>{job.title}</h3>
+      <div
+        className={`flex flex-col hover:bg-slate-50 p-3 ${
+          job.id === selectedJob.id ? "bg-slate-400" : ""
+        }`}
+      >
+        <h3 className="font-bold">{job.title}</h3>
+        <p>Bounty: ${job.bountyUSD} USD</p>
       </div>
     </div>
   );
@@ -15,12 +27,17 @@ const JobView = () => {
   const [setSelectedJob, selectedJob] = useState(null);
 
   return (
-    <div className="bg-white flex-1 w-full h-full m-3 p-3">
+    <div className="bg-white flex-1 w-full h-full m-3">
       <h3 className="font-bold text-2xl text-center">Select a job...</h3>
 
       <div className="flex flex-col">
-        {mockData.jobPostings.map((job) => (
-          <JobPost job={job} key={job.id} onPress={() => setSelectedJob(job)} />
+        {mockData.jobPostings.map((job: ExpandedJobPosting) => (
+          <JobPost
+            job={job}
+            key={job.id}
+            selectedJob={selectedJob}
+            onPress={() => setSelectedJob(job)}
+          />
         ))}
       </div>
     </div>
