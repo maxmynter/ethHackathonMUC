59b56a7a93db657160aeb440f6b92a25a9151a3a
owen <owen@owen>
2023-08-12 17:45:08 +0200

    basic match screen

--- pages/linkmatch/index.tsx
+++ pages/linkmatch/index.tsx
@@ -1,15 +1,27 @@
 import Header from "../../components/header";
 const LinkMatchView = () => {
   return (
-    <div>
-      <Header />
+    <div className="flex flex-col  h-full">
+      <div>
+        <Header />
+      </div>
 
-      <div className=" bg-slate-100">
-        <div className="content grid gap-4 grid-cols-2 grid-rows-1">
-          <div className="bg-white flex-1 w-full m-3">Job here</div>
-          <div className="bg-white flex-1 w-full m-3">Candidate here</div>
+      <div className="mt-3 bg-slate-100 flex-1">
+        <div className="container mx-auto grid gap-4 grid-cols-2 grid-rows-1">
+          <div className="bg-white flex-1 w-full m-3 p-3">Job here</div>
+          <div className="bg-white flex-1 w-full m-3 p-3">Candidate here</div>
         </div>
       </div>
+
+      <style global jsx>{`
+        html,
+        body,
+        body > div:first-child,
+        div#__next,
+        div#__next > div {
+          height: 100%;
+        }
+      `}</style>
     </div>
   );
 };
