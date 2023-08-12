08f431bd2c11a2be9135ebb10ad8b20d10b0b219
owen <owen@owen>
2023-08-12 20:07:01 +0200

    jobview

---
+++ pages/linkmatch/JobView.tsx
@@ -0,0 +1,13 @@
+import mockData from "../../utils/mockData";
+
+const JobView = () => {
+  return (
+    <div className="bg-white flex-1 w-full h-full m-3 p-3">
+      <h3 className="font-bold text-2xl text-center">Select a job...</h3>
+
+      <div className="flex flex-col"></div>
+    </div>
+  );
+};
+
+export default JobView;
