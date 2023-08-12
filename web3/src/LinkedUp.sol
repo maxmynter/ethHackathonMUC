823a3b746b64e6336943ce02aab75c730b5500c2
Lucas <0Zeta@protonmail.com>
2023-08-12 12:06:34 +0200

    Add vault address + ownership of LinkedUp contract

--- web3/src/LinkedUp.sol
+++ web3/src/LinkedUp.sol
@@ -1,6 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract LinkedUp {
-    
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract LinkedUp is Ownable {
+
+    address payable VAULT;
+
+    event VaultChanged(address indexed _from, address indexed _to, address indexed by);
+
+    constructor (address payable _vault) {
+        VAULT = _vault;
+        emit VaultChanged(address(0), _vault, owner());
+    }
+
+    function changeVaultAddress(address payable _vault) onlyOwner public {
+        require(_vault != address(0), "Zero address");
+        address old = VAULT;
+        VAULT = _vault;
+        emit VaultChanged(old, _vault, msg.sender);
+    }
 }
