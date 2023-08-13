160868aca25530ad31baffb2a120f9cea9044d11
owen <owen@owen>
2023-08-13 07:27:31 +0200

    cleanup code

--- pages/company/JobPostingForm.tsx
+++ pages/company/JobPostingForm.tsx
@@ -9,461 +9,7 @@ import {
 
 const LUaddress = "0x76200A7A3f647C64d7ec3ce0D2df2D5Ae804A81c";
 
-const abi = [
-  {
-    "inputs": [
-      {
-        "internalType": "address payable",
-        "name": "_vault",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "_proposalFee",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "_bounty",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "deployer",
-        "type": "address"
-      },
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "applicantProfile",
-        "type": "address"
-      }
-    ],
-    "name": "ApplicantProfileCreation",
-    "type": "event"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "_from",
-        "type": "uint256"
-      },
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "_to",
-        "type": "uint256"
-      },
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "by",
-        "type": "address"
-      }
-    ],
-    "name": "MinimumMatchProposalFeeChanged",
-    "type": "event"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "_from",
-        "type": "uint256"
-      },
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "_to",
-        "type": "uint256"
-      },
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "by",
-        "type": "address"
-      }
-    ],
-    "name": "MinimumOfferBountyChanged",
-    "type": "event"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "poster",
-        "type": "address"
-      },
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "offer",
-        "type": "address"
-      },
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "bounty",
-        "type": "uint256"
-      },
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "minBet",
-        "type": "uint256"
-      },
-      {
-        "indexed": false,
-        "internalType": "uint16",
-        "name": "numberOfWinners",
-        "type": "uint16"
-      },
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "duration",
-        "type": "uint256"
-      }
-    ],
-    "name": "OfferCreation",
-    "type": "event"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "previousOwner",
-        "type": "address"
-      },
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "newOwner",
-        "type": "address"
-      }
-    ],
-    "name": "OwnershipTransferred",
-    "type": "event"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "_from",
-        "type": "address"
-      },
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "_to",
-        "type": "address"
-      },
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "by",
-        "type": "address"
-      }
-    ],
-    "name": "VaultChanged",
-    "type": "event"
-  },
-  {
-    "inputs": [],
-    "name": "MAX_DURATION",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "MIN_DURATION",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "MIN_MATCH_PROPOSAL_FEE",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "MIN_OFFER_BOUNTY",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "VAULT",
-    "outputs": [
-      {
-        "internalType": "address payable",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "_fee",
-        "type": "uint256"
-      }
-    ],
-    "name": "changeMinimumMatchProposalFee",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "_bounty",
-        "type": "uint256"
-      }
-    ],
-    "name": "changeMinimumOfferBounty",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address payable",
-        "name": "_vault",
-        "type": "address"
-      }
-    ],
-    "name": "changeVaultAddress",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "createApplicantProfile",
-    "outputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "bytes32[]",
-        "name": "_data",
-        "type": "bytes32[]"
-      }
-    ],
-    "name": "createApplicantProfileWithData",
-    "outputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "bytes32",
-        "name": "_data",
-        "type": "bytes32"
-      },
-      {
-        "internalType": "uint256",
-        "name": "_bounty",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "_minBet",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint16",
-        "name": "_nWinners",
-        "type": "uint16"
-      },
-      {
-        "internalType": "uint256",
-        "name": "_duration",
-        "type": "uint256"
-      }
-    ],
-    "name": "createOffer",
-    "outputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "stateMutability": "payable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "offer",
-        "type": "uint256"
-      }
-    ],
-    "name": "deleteOffer",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "name": "deployerOfApplicant",
-    "outputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "numberOfOffers",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "offers",
-    "outputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "owner",
-    "outputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "renounceOwnership",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "newOwner",
-        "type": "address"
-      }
-    ],
-    "name": "transferOwnership",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "stateMutability": "payable",
-    "type": "receive"
-  }
-] as const;
+const abi = require("./abi.json");
 
 const JobPostingForm = () => {
   const [usdAmount, setUsdAmount] = useState("");
@@ -472,9 +18,10 @@ const JobPostingForm = () => {
   const [jobTitle, setJobTitle] = useState("");
   const [companyName, setCompanyName] = useState("");
 
-  const { address, connector, isConnected } = useAccount()
+  const { address, connector, isConnected } = useAccount();
 
-  const { config,
+  const {
+    config,
     error: prepareError,
     isError: isPrepareError,
   } = usePrepareContractWrite({
@@ -482,16 +29,22 @@ const JobPostingForm = () => {
     abi: abi,
     functionName: "createOffer",
     // Post some mock data for now
-    args: ["0x0123456789012345678901234567890001234567890123456789012345678901", BigInt(1000000000000), BigInt(10000000000), 1, BigInt(10000000)],
+    args: [
+      "0x0123456789012345678901234567890001234567890123456789012345678901",
+      BigInt(1000000000000),
+      BigInt(10000000000),
+      1,
+      BigInt(10000000),
+    ],
     account: address,
-    value: BigInt(1000000000000)
-  })
+    value: BigInt(1000000000000),
+  });
 
-  const { data, error, isError, write } = useContractWrite(config)
+  const { data, error, isError, write } = useContractWrite(config);
 
   const { isLoading, isSuccess } = useWaitForTransaction({
     hash: data?.hash,
-  })
+  });
 
   const usdToEthConversionRate = USD_TO_ETH;
 
@@ -511,15 +64,23 @@ const JobPostingForm = () => {
     );
   };
 
-  const handleSubmit = () => {
-    console.log("Handle Submit of Form");
-    console.log(usdAmount, ethAmount, jobDescription, jobTitle, companyName);
-    write();
+  const clearForm = () => {
     setUsdAmount(0);
     setEthAmount(0);
     setJobDescription("");
     setJobTitle("");
     setCompanyName("");
+  };
+
+  const handleSubmit = () => {
+    console.log(usdAmount, ethAmount, jobDescription, jobTitle, companyName);
+    try {
+      write();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      clearForm();
+    }
   };
 
   return (
